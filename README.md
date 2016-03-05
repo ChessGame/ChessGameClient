125169c991a9451e34cf638318309fb8da2783bf
xiaomo <83387856@qq.com>
2016-03-05 18:02:57 +0800

    加载组件

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+ 浙ICP备15009606号
  #1 下载依赖
  npm install
  #2 启动程序
@@ -7,4 +8,3 @@
  import 的时候 用 {}包里来，里面写具体的某个方法，就可以拿来用了。
 例如  import {render} from ‘react-dom’   直接用render()
  import ReactDOM from 'react-dom'  需要用ReactDOM.render()来调用内部方法
-#
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './src/components/example/app';
-// import 的时候 用 {}包里来，里面写具体的某个方法，就可以拿来用了。
-// 例如  import {render} from ‘react-dom’   直接用render()
-// import ReactDOM from 'react-dom'  需要用ReactDOM.render()来调用内部方法
-ReactDOM.render(<App />, document.getElementById('root'));
+import { NavBar } from './src/components/common/NavBarComponent';
+
+ReactDOM.render(<NavBar />, document.getElementById('root'));
