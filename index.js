15fdaba82fbaa8c49e1eabf6fd8dafd2776e28e6
xiaomo <83387856@qq.com>
2016-03-05 17:01:40 +0800

    组件框架

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './src/app';
+import { App } from './src/components/example/app';
 // import 的时候 用 {}包里来，里面写具体的某个方法，就可以拿来用了。
 // 例如  import {render} from ‘react-dom’   直接用render()
 // import ReactDOM from 'react-dom'  需要用ReactDOM.render()来调用内部方法
