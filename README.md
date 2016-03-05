35d269f769ba00403bfde3eff8d1b1c0f42b75e2
xiaomo <83387856@qq.com>
2016-03-05 16:12:26 +0800

    demo搭建完成

---
+++ README.md
@@ -0,0 +1,10 @@
+ #1 下载依赖
+ npm install
+ #2 启动程序
+ npm start
+ 访问路径：http://localhost:8080
+ 
+ import 的时候 用 {}包里来，里面写具体的某个方法，就可以拿来用了。
+例如  import {render} from ‘react-dom’   直接用render()
+ import ReactDOM from 'react-dom'  需要用ReactDOM.render()来调用内部方法
+#
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-var Test = React.createClass({render: function () {
-    return (
-        <div>Test</div>
-    )
-}});
-ReactDOM.render(<Test/>,document.body);
+import { App } from './src/app';
+// import 的时候 用 {}包里来，里面写具体的某个方法，就可以拿来用了。
+// 例如  import {render} from ‘react-dom’   直接用render()
+// import ReactDOM from 'react-dom'  需要用ReactDOM.render()来调用内部方法
+ReactDOM.render(<App />, document.getElementById('root'));
