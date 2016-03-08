3058c916a711c59a2dae51214bde0d5f2b35350d
xiaomo <83387856@qq.com>
2016-03-08 12:11:06 +0800

    react redux

--- src/components/common/NavBarComponent.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-export class NavBar extends Component {
-    render() {
-        return (
-            <div className="container">© 2015-2016 xiaomo 浙ICP备15009606号 xiaomo.info.cn 更新日志</div>
-        );
-    }
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,47 @@
-var webpack = require('webpack');
 var path = require('path');
+var webpack = require('webpack');
 var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
 var hotModuleReplacementPlugin = new webpack.HotModuleReplacementPlugin();
 var noErrorsPlugin = new webpack.NoErrorsPlugin();
 module.exports = {
-    entry: "./index.js",
-    output: {
-        path: path.join(__dirname, '/build/'),
-        filename: 'bundle.js',
-        publicPath: "/build/"
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015', 'react']
-                }
-            },
-
-            {
-                test: /\.css$/,
-                loaders: ['style', 'css']
-            },
-
-            {
-                test: /\.(gif|jpg|png|woff|woff2|svg|eot|ttf)$/,
-                loaders: ['url-loader?limit=8192']
-            }
-        ]
-    },
-    plugins: [
-        commonsPlugin,
-        hotModuleReplacementPlugin,
-        noErrorsPlugin
-    ],
-    devServer: {
-        historyApiFallback: true,
-        hot: true,
-        inline: true,
-        progress: true
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, '/build/'),
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  },
+  plugins: [
+    hotModuleReplacementPlugin,
+    noErrorsPlugin
+  ],
+  resolve: {
+    alias: {
+      'redux-devtools': path.join(__dirname, 'src', 'ext'),
+      'react': path.join(__dirname, 'node_modules', 'react')
     }
+  },
+  resolveLoader: {
+    'fallback': path.join(__dirname, 'node_modules')
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel'],
+      exclude: /node_modules/,
+      include: path.join(__dirname, 'src')
+    }, {
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'src', 'ext')
+    },{
+        test: /\.(gif|jpg|png|woff|woff2|svg|eot|ttf)$/,
+        loaders: ['url-loader?limit=8192']
+      }
+    ]
+  }
 };
