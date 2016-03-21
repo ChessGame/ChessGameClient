5409d57d8313ea7a91bd1d54347186c5dc3597a7
xiaomo <83387856@qq.com>
2016-03-21 16:07:37 +0800

    引入css样式

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import {render} from "react-dom";
 import configureStore from "./store/configureStore";
 import Root from "./containers/root/Root";
 import "babel-polyfill";
-import "slick-carousel/slick/slick.css";
 const store = configureStore();
 
 render(
