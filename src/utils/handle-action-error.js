8bd3f7033815f2b08cca59d8034a1b384327fa97
xiaomo <83387856@qq.com>
2016-03-24 13:23:22 +0800

    html5+css3初始化环境

--- src/utils/handle-action-error.js
+++
@@ -1,9 +0,0 @@
-import { SHOW_ERROR } from '../constants/ActionTypes';
-
-export default function handleActionError(dispatch, error, source) {
-    return dispatch({
-        type: SHOW_ERROR,
-        source,
-        payload: error
-    })
-}
